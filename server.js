f92e1c590be424ee0894f52b7b74bca56b160bea
Adil Alam <YOUR_GMAIL@gmail.com>
2026-08-30 18:44:28 +0530

    Fix Render port binding

--- server.js
+++ server.js
@@ -624,14 +624,3 @@ app.listen(PORT, "0.0.0.0", () => {
     console.log(
       "----------------------------------"
     );
-
-    console.log(
-      `Server running on http://127.0.0.1:${PORT}`
-    );
-
-    console.log(
-      "----------------------------------"
-    );
-
-  }
-};
